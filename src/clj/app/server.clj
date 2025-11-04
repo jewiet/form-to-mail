5f4eef72f62190b1d3b88b5207fbe3a5d8ea4e85
Fana Mehari <fannamehari@gmail.com>
2025-11-04 17:13:38 +0100

    Implement routing to satisfy all specs
    Now we have two routes,
    - home-page
    - form-submission

--- src/clj/app/server.clj
+++ src/clj/app/server.clj
@@ -2,6 +2,7 @@
   (:require
    [aleph.http :as http]
    [clojure.string :as string]
+   [clojure.pprint :refer [pprint]]
    [ring.middleware.params :refer [wrap-params]]))
 
 
@@ -15,16 +16,34 @@
       (do (println (str "Form submitted by " email))
           (println (str "message: " message))
           {:status  200
-           :headers {"Content-Type" "text/html"}
-           :body    "Hello form,  We love Nix!"})
+           :headers {"Content-Type" "text/plain"}
+           :body    (str "Thank you for sending the form. We have sent you an email with confirmation link to " email)})
       (do
         (println "Missing required field email")
-        {:status  200
-         :headers {"Content-Type" "text/html"}
+        {:status  422
+         :headers {"Content-Type" "text/plain"}
          :body  "Missing required field email"}))))
 
+(defn home-handler
+  [_request]
+  {:status  200
+   :headers {"Content-Type" "text/plain"}
+   :body "Hello, form!"})
+
+(defn router
+  [request]
+  (let [route (select-keys request [:uri :request-method])]
+    (case route
+      {:uri "/"
+       :request-method :get} (home-handler request)
+      {:uri "/poc-submit"
+       :request-method :post} (form-handler request)
+      {:status  404
+       :headers {"Content-Type" "text/plain"}
+       :body "Not Found"})))
+
 (def http-handler
-  (-> form-handler
+  (-> router
       wrap-params))
 
 (defn start
