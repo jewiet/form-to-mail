63fb230f414f4bcf604eb1f6265c03ba17a20757
F.M. Hagos <fana@Mac.home>
2025-11-13 16:35:44 +0100

    Implement confirmation of submission spec

--- src/clj/app/server.clj
+++ src/clj/app/server.clj
@@ -1,22 +1,45 @@
 (ns app.server
   (:require
-    [clojure.string :as string]
-    [io.pedestal.connector :as conn]
-    ;; [clojure.pprint :refer [pprint]]
-    [io.pedestal.http.http-kit :as hk]))
+   [clojure.string :as string]
+   [io.pedestal.connector :as conn]
+   [clojure.pprint :refer [pprint]]
+   [io.pedestal.http.http-kit :as hk]))
 
+(defonce submissions (atom {}))
+
+
+;; TODO: Implement
+(defn submission-verification [{:keys [path-params]}]
+  (pprint @submissions)
+  (if-let [submission-uuid (parse-uuid (:submission-uuid path-params))]
+    (do
+      (println "Verifying submission id" submission-uuid)
+      (if-let [submission (get @submissions submission-uuid)]
+        (do
+          (doseq [param (dissoc submission "email")]
+            (println (str (key param) ": " (val param)))
+            submission)
+          {:status  200
+           :headers {"Content-Type" "text/plain"}
+           :body  "Thank you for confirmation. Your form is delivered."})
+        {:status  404
+         :headers {"Content-Type" "text/plain"}
+         :body    "Submission not found"}))
+    {:status  422
+     :headers {"Content-Type" "text/plain"}
+     :body    "Invalid submission uuid"}))
 
 (defn form-handler
   [{:keys [params]}]
   (let [email   (get params "email")]
     (if-not (string/blank? email)
-      (do (println (str "Form submitted by " email))
-          (doseq [param (dissoc params "email")]
-            (println (str (key param) ": " (val param)))
-               params)
-          {:status  200
-           :headers {"Content-Type" "text/plain"}
-           :body    (str "Thank you for sending the form. We have sent you an email with confirmation link to " email)})
+      (let [submission-uuid (random-uuid)]
+        (println (str "Form submitted by " email))
+        (swap! submissions assoc submission-uuid params)
+        (println (str "Sending confirmation link: http://localhost:8080/confirm-submission/" submission-uuid))
+        {:status  200
+         :headers {"Content-Type" "text/plain"}
+         :body    (str "Thank you for sending the form. We have sent you an email with confirmation link to " email)})
       (do
         (println "Missing required field email")
         {:status  422
@@ -32,8 +55,15 @@
 
 
 (def routes
-  #{["/" :get home-handler :route-name :home]
-    ["/poc-submit" :post form-handler :route-name :form-submit]})
+  #{["/"
+     :get home-handler
+     :route-name :home]
+    ["/poc-submit"
+     :post form-handler
+     :route-name :form-submit]
+    ["/confirm-submission/:submission-uuid"
+     :get submission-verification
+     :route-name :submission-verification]})
 
 (defn log-connector [{:keys [host port] :as connector-map}]
   (-> (str "Starting Form to Mail on " host ":" port)
