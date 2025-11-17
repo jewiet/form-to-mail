f4a8f4dc7530151b9411b7321c1b82127b120b8c
F.M. Hagos <fana@Mac.home>
2025-11-17 18:26:22 +0100

    TBB interpreter: Run step implementation if exists
    Extract variant and arguments from an execute message. Get
    implementation from the steps-implementation map by the variant.

--- src/http_spec_interpreter.clj
+++ src/http_spec_interpreter.clj
@@ -5,9 +5,22 @@
    ;; [clojure.pprint :refer [pprint]]
    ))
 
+(def steps-implementation
+  {"Make a {0} request to {1}."
+   (fn [method url] (.println *err* "running step"))})
+
 (println (json/write-str {:type "InterpreterState"
                           :ready true}))
+
 (doseq [line (line-seq (java.io.BufferedReader. *in*))]
   (when-not (blank? line)
-    (.println *err* (json/read-str line :key-fn keyword))
-    (println (json/write-str {:type "Success"}))))
+    (let [message       (json/read-str line :key-fn keyword)
+          variant       (get-in message [:step :variant])
+          arguments     (get-in message [:step :arguments])
+          implmentation (get steps-implementation variant)]
+      (.println *err* message)
+      (if (nil? implmentation)
+        (println (json/write-str {:type   "Failure"
+                                  :reason "Not implemented"}))
+        (do (apply implmentation arguments)
+            (println (json/write-str {:type "Success"})))))))
