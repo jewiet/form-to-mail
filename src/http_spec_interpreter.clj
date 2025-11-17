2bfffb1ff0409367b5ccf824b1d9d46eaf21beac
F.M. Hagos <fana@Mac.home>
2025-11-17 11:09:31 +0100

    Introduce clojure.data.json

--- src/http_spec_interpreter.clj
+++ src/http_spec_interpreter.clj
@@ -1,6 +1,13 @@
-(ns http-spec-interpreter)
+(ns http-spec-interpreter
+  (:require
+   [clojure.data.json :as json]
+   [clojure.string :refer [blank?]]
+   ;; [clojure.pprint :refer [pprint]]
+   ))
 
-(println "{ \"type\": \"InterpreterState\", \"ready\": true }")
-;; TODO: Read all lines
+(println (json/write-str {:type "InterpreterState"
+                          :ready true}))
 (doseq [line (line-seq (java.io.BufferedReader. *in*))]
-    (println "{ \"type\": \"Success\"}"))
+  (when-not (blank? line)
+    (.println *err* (json/read-str line :key-fn keyword))
+    (println (json/write-str {:type "Success"}))))
