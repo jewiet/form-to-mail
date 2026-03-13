587595365632099fe804c31afacadaf7abedc3ab
Tad Lispy <tadeusz@lazurski.pl>
2026-03-13 15:50:12 +0100

    Use clojure.core.logging in tbb/core
    Instead of pedestal logging. This is in preparation to move tbb.core
    from the Form to Mail project into TBB itself.

--- src/clj/tbb/core.clj
+++ src/clj/tbb/core.clj
@@ -3,7 +3,10 @@
    [clojure.data.json :as json]
    [clojure.pprint :refer [pprint]]
    [clojure.string :refer [blank?]]
-   [io.pedestal.log :refer [debug error info]]))
+   [clojure.tools.logging.readable :as logging]))
+
+;; TODO: Make it work with Babashka (so no dependency on JDK)
+;; TODO: Distribute this with TBB so it can be used in other Clojure projects.
 
 (defonce ^:private steps-implementation (atom {}))
 
@@ -29,7 +32,7 @@
             variant       (:variant step)
             arguments     (:arguments step)
             implmentation (get @steps-implementation variant)]
-        (debug :prose "got a message from tbb" :message message)
+        (logging/debug "got a message from tbb" message)
         (if (nil? implmentation)
           (let [suggestion
                 `(tbb/implement-step
@@ -41,7 +44,7 @@
                          data]
                         (tis ~'= "cat" "dog")))]
 
-            (error :missing-step-implementation variant)
+            (logging/error "missing step implementation" variant)
             (println (json/write-str {:type   "Failure"
                                       :reason "Not implemented"
                                       :hint (str "To get started put this in your interpreter:\n\n"
@@ -55,4 +58,4 @@
               (println (json/write-str {:type   "Failure"
                                         :reason (.getMessage e)}))))))))
 
-  (debug :prose "Done reading from tbb."))
+  (logging/debug "Done reading from tbb."))
